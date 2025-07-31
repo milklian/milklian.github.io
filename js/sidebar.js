d10f9065f4e927b0d98b5285c449b9ac1c729f76
milklian <dingyi1520@163.com>
2025-07-31 21:20:27 +0800

    Site updated: 2025-07-31 21:20:27

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,50 @@
-function u(o){if(o.classList.contains("active-current"))return;document.querySelectorAll(".post-toc .active").forEach(s=>{s.classList.remove("active","active-current")}),o.classList.add("active","active-current");let c=o.parentNode;for(;!c.matches(".post-toc");)c.matches("li")&&c.classList.add("active"),c=c.parentNode}function f(){let o=document.querySelectorAll(".post-toc li");if(!o.length)return;let c=[...o].map(t=>{let e=t.querySelector(".toc-link"),n=document.getElementById(decodeURI(e.getAttribute("href")).replace("#",""));return e.addEventListener("click",a=>{a.preventDefault(),window.scrollTo(0,n.offsetTop+1)}),n});function s(t){let e=0,n=t[e];if(n.boundingClientRect.top>0)return e=c.indexOf(n.target),e===0?0:e-1;for(;e<t.length;e++)if(t[e].boundingClientRect.top<=0)n=t[e];else return c.indexOf(n.target);return c.indexOf(n.target)}function r(t){t=Math.floor(t+1e4);let e=new IntersectionObserver((n,a)=>{let i=document.documentElement.scrollHeight+100;if(i>t){a.disconnect(),r(i);return}let l=s(n);u(o[l])},{rootMargin:`${t}px 0px -100% 0px`,threshold:0});c.forEach(n=>{n&&e.observe(n)})}r(document.documentElement.scrollHeight)}function d(){let o="sidebar-nav-active",c="sidebar-panel-active";function s(){let t=document.querySelector(".sidebar-nav-toc"),e="ri:list-ordered",n="ri:list-unordered";t&&t.addEventListener("click",()=>{if(console.log("click"),t.classList.contains(o)){let i=t.querySelector(".iconify");i.dataset.icon=i.dataset.icon===e?n:e,document.querySelectorAll(".toc-number").forEach(l=>{l.classList.toggle("hidden")})}})}function r(){document.querySelectorAll(".sidebar-nav li").forEach(e=>{e.onclick=function(){this.classList.contains(o)||(document.querySelector(`.${c}`).classList.remove(c),document.querySelector(`#${this.dataset.target}`).classList.add(c),document.querySelector(`.${o}`).classList.remove(o),this.classList.add(o))}})}s(),r(),f()}document.addEventListener("DOMContentLoaded",d);document.addEventListener("pjax:success",d);
+/* global CONFIG */
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const isRight = CONFIG.sidebar.position === 'right';
+
+  const sidebarToggleMotion = {
+    mouse: {},
+    init() {
+      window.addEventListener('mousedown', this.mousedownHandler.bind(this));
+      window.addEventListener('mouseup', this.mouseupHandler.bind(this));
+      document.querySelector('.sidebar-dimmer').addEventListener('click', this.clickHandler.bind(this));
+      document.querySelector('.sidebar-toggle').addEventListener('click', this.clickHandler.bind(this));
+      window.addEventListener('sidebar:show', this.showSidebar);
+      window.addEventListener('sidebar:hide', this.hideSidebar);
+    },
+    mousedownHandler(event) {
+      this.mouse.X = event.pageX;
+      this.mouse.Y = event.pageY;
+    },
+    mouseupHandler(event) {
+      const deltaX = event.pageX - this.mouse.X;
+      const deltaY = event.pageY - this.mouse.Y;
+      const clickingBlankPart = Math.hypot(deltaX, deltaY) < 20 && event.target.matches('.main');
+      // Fancybox has z-index property, but medium-zoom does not, so the sidebar will overlay the zoomed image.
+      if (clickingBlankPart || event.target.matches('img.medium-zoom-image')) {
+        this.hideSidebar();
+      }
+    },
+    clickHandler() {
+      document.body.classList.contains('sidebar-active') ? this.hideSidebar() : this.showSidebar();
+    },
+    showSidebar() {
+      document.body.classList.add('sidebar-active');
+      const animateAction = isRight ? 'fadeInRight' : 'fadeInLeft';
+      document.querySelectorAll('.sidebar .animated').forEach((element, index) => {
+        element.style.animationDelay = (100 * index) + 'ms';
+        element.classList.remove(animateAction);
+        setTimeout(() => {
+          // Trigger a DOM reflow
+          element.classList.add(animateAction);
+        });
+      });
+    },
+    hideSidebar() {
+      document.body.classList.remove('sidebar-active');
+    }
+  };
+  sidebarToggleMotion.init();
+});
